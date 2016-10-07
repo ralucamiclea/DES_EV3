d1ee06f6a306923814587055635b24053d7263aa
Flip <itsvizu@gmail.com>
2016-10-07 15:45:43 +0200

    AvoidCollision and Driveforward Behaviors

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,8 @@
-
-
-import lejos.hardware.BrickFinder;
-import lejos.hardware.Button;
-import lejos.hardware.Sound;
-import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.lcd.Font;
-import lejos.hardware.lcd.GraphicsLCD;
-import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
-import lejos.hardware.sensor.NXTLightSensor;
 import lejos.robotics.RegulatedMotor;
-import lejos.robotics.SampleProvider;
-import lejos.utility.Delay;
-
-
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
 
 /**
  * @author raluca.miclea
@@ -27,42 +15,14 @@ public class Main {
 	 */
 	 public static void main(String[] args)
 	    {
-	       /* //System.out.println("Running...");
-	        GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
-	        final int SW = g.getWidth();
-	        final int SH = g.getHeight();
-	        Button.LEDPattern(4);
-	        Sound.beepSequenceUp();
-	        
-	        g.setFont(Font.getLargeFont());
-	        g.drawString("leJOS/EV3", SW/2, SH/2, GraphicsLCD.BASELINE|GraphicsLCD.HCENTER);
-	        Button.LEDPattern(3);
-	        Delay.msDelay(4000);
-	        Button.LEDPattern(5);
-	        g.clear();
-	        g.refresh();
-	        Sound.beepSequence();
-	        Delay.msDelay(500);
-	        Button.LEDPattern(0);*/
-	        
-	        RegulatedMotor lm = new EV3LargeRegulatedMotor(MotorPort.A);
-	        RegulatedMotor rm = new EV3LargeRegulatedMotor(MotorPort.D);
-	        lm.forward();
-
-	        NXTLightSensor lightSensor = new NXTLightSensor(LocalEV3.get().getPort("S2"));
-	        SampleProvider light = lightSensor.getRedMode();
-	        float[] lightSamples = new float[light.sampleSize()];
-	        while(!Button.ESCAPE.isDown()){
-	        	light.fetchSample(lightSamples,0);
-	        	LCD.drawString("LI: " + lightSamples[0],0,2);
-	        	if(lightSamples[0]>0.5){
-	        		rm.forward();
-	        		Delay.msDelay(50);
-	        	}else{
-	        		rm.backward();
-	        		Delay.msDelay(50);
-	        	}
-	        }
+		 RegulatedMotor lm = new EV3LargeRegulatedMotor(MotorPort.A);
+		 RegulatedMotor rm = new EV3LargeRegulatedMotor(MotorPort.D);
+		 Behavior B1 = new DriveForward(rm,lm);
+		 Behavior B2 = new AvoidCollision("S3", "S1", rm, lm);
+		 
+		 Behavior [] bArray = {B1, B2};
+		 Arbitrator arby = new Arbitrator(bArray);
+	     arby.start();
 	    }
 }
 
