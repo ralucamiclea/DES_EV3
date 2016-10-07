2b0ff2f3d849275dc2e4117dc0374cdb7f496c5b
ralucamiclea <rmiclea02@gmail.com>
2016-10-07 12:57:18 +0200

    MainClass

---
+++ src/Assignment1/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Assignment1;
+
+/**
+ * @author raluca.miclea
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
