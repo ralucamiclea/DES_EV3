35e7bb7dbf548fd7e0eadd8d75ec20db7e8b8529
ralucamiclea <rmiclea02@gmail.com>
2016-10-14 11:20:00 +0200

    main for assignment 2

---
+++ src/assignment2/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package assignment2;
+
+/**
+ * @author raluca.miclea
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
